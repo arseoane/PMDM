4b8126ad8b429e726ad20a2996991a66d3450282
arseoane <arodriguezseoane@danielcastelao.org>
2026-09-23 13:02:15 +0200

    Codelab 2

--- src/example/myapp/Aquarium.kt
+++ src/example/myapp/Aquarium.kt
@@ -1,13 +1,11 @@
 package example.myapp
 
-class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
-    var width: Int = 20
-    var height: Int = 40
-    var length: Int = 100
-
-    fun printSize() {
-        println("Width: $width cm " +
-                "Length: $length cm " +
-                "Height: $height cm ")
+class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+    init {
+        println("aquarium initializing")
+    }
+    init {
+        // 1 liter = 1000 cm^3
+        println("Volume: ${width * length * height / 1000} l")
     }
 }
