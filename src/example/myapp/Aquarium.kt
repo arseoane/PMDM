ce7f2da3fbff3251fd9b33bf6dae52e0e47293f4
arseoane <arodriguezseoane@danielcastelao.org>
2026-09-23 12:57:32 +0200

    Codelab 2

--- src/example/myapp/Aquarium.kt
+++ src/example/myapp/Aquarium.kt
@@ -1,6 +1,6 @@
 package example.myapp
 
-class Aquarium {
+class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
     var width: Int = 20
     var height: Int = 40
     var length: Int = 100
