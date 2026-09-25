7ebb0726619ab7200df5714ff7523aad2173dc03
arseoane <arodriguezseoane@danielcastelao.org>
2026-09-25 10:43:00 +0200

    Codelab 2

--- src/example/myapp/Aquarium.kt
+++ src/example/myapp/Aquarium.kt
@@ -1,6 +1,17 @@
 package example.myapp
 
-class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+open class Aquarium (open var length: Int = 100, open var width: Int = 20, open var height: Int = 40) {
+    open var volume: Int
+        get() = width * height * length / 1000
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
+
+    open val shape = "rectangle"
+
+    open var water: Double = 0.0
+        get() = volume * 0.9
+
     init {
         println("aquarium initializing")
     }
@@ -10,8 +21,11 @@ class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40
     }
 
     fun printSize() {
+        println(shape)
         println("Width: $width cm " +
                 "Length: $length cm " +
                 "Height: $height cm ")
+        // 1 l = 1000 cm^3
+        println("Volume: $volume l Water: $water l (${water/volume*100.0}% full)")
     }
 }
