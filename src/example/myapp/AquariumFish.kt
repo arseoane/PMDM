907ea163e8686e8aed00e71c796d1a9a6e99d8d8
arseoane <arodriguezseoane@danielcastelao.org>
2026-09-25 11:17:33 +0200

    Codelab 2

---
+++ src/example/myapp/AquariumFish.kt
@@ -0,0 +1,50 @@
+package example.myapp
+
+abstract class AquariumFish {
+    abstract val color: String
+}
+
+class Shark: AquariumFish(), FishAction {
+    override val color = "gray"
+    override fun eat() {
+        println("hunt and eat fish")
+    }
+}
+
+class Plecostomus: AquariumFish(), FishAction {
+    override val color = "gold"
+    override fun eat() {
+        println("eat algae")
+    }
+}
+
+fun makeFish() {
+    val shark = Shark()
+    val pleco = Plecostomus()
+    println("Shark: ${shark.color}")
+    shark.eat()
+    println("Plecostomus: ${pleco.color}")
+    pleco.eat()
+}
+
+fun main () {
+    makeFish()
+}
+
+interface FishAction  {
+    fun eat()
+}
+
+interface AquariumAction {
+    fun eat()
+    fun jump()
+    fun clean()
+    fun catchFish()
+    fun swim()  {
+        println("swim")
+    }
+}
+
+interface FishColor {
+    val color: String
+}
